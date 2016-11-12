ba74eaa791780483eb4f854cc70b336716d4379d
Valerie Chen <valeriechen@Valeries-MBP.wireless.yale.edu>
2016-11-11 21:26:02 -0500

    initial commit

---
+++ Pods/Target Support Files/Pods-ffsTests/Pods-ffsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ffsTests : NSObject
+@end
+@implementation PodsDummy_Pods_ffsTests
+@end
---
+++ Pods/Target Support Files/Pods-ffsUITests/Pods-ffsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ffsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ffsUITests
+@end
